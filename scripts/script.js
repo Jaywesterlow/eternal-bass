7fe058d5c9213bca8a07d6ff43774ee31c4632b9
Jaymar Westerlow <jaywesterlow71@gmail.com>
2024-01-18 01:46:27 +0100

    FAQ updated & bugfixes

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,42 @@
+const faqItem1 = document.getElementById('faqItem1');
+const faqItem2 = document.getElementById('faqItem2');
+const faqItem3 = document.getElementById('faqItem3');
+const faqItem4 = document.getElementById('faqItem4');
+const faqItemContent1 = document.getElementById('faqContent1');
+const faqItemContent2 = document.getElementById('faqContent2');
+const faqItemContent3 = document.getElementById('faqContent3');
+const faqItemContent4 = document.getElementById('faqContent4');
+
+faqItem1.addEventListener('click', toggleContent1);
+
+function toggleContent1() {
+  faqItemContent1.classList.toggle('open')
+}
+faqItem2.addEventListener('click', toggleContent2);
+
+function toggleContent2() {
+  faqItemContent2.classList.toggle('open')
+}
+faqItem3.addEventListener('click', toggleContent3);
+
+function toggleContent3() {
+  faqItemContent3.classList.toggle('open')
+}
+faqItem4.addEventListener('click', toggleContent4);
+
+function toggleContent4() {
+  faqItemContent4.classList.toggle('open')
+}
+
+
+
+
+
+
+
+
+
+
 var swiper = new Swiper(".swiper", {
   effect: "coverflow",
   grabCursor: true,
